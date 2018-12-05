c9cd54d4da1c810b55a6a2219506598d9646f597
Spark1er <ricksanford498@gmail.com>
2018-12-05 09:00:36 +0700

    first commit

---
+++ main2.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('./G4Sr4Cld-6c.jpg', 0)
+img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]  # ensure binary
+cv2.imshow('labeledt.png', img)
+ret, labels = cv2.connectedComponents(img)
+
+# Map component labels to hue val
+label_hue = np.uint8(99999999*labels/np.max(labels))
+blank_ch = 255*np.ones_like(label_hue)
+labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+
+# cvt to BGR for display
+labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+
+# set bg label to black
+labeled_img[label_hue == 1] = 0
+
+cv2.imshow('labeled.png', labeled_img)
+cv2.waitKey()
